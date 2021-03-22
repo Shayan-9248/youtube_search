2a9862bc3ee71c42ffd94956c1d915c190a17f45
Shayan-9248 <shayan.aimoradii@gmail.com>
2021-03-22 17:54:22 +0430

    end project

--- youtube/views.py
+++ youtube/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.conf import settings
 from isodate import parse_duration
@@ -6,43 +6,48 @@ import requests
 
 
 def index(request):
-    search_url = 'https://www.googleapis.com/youtube/v3/search'
-    video_url = 'https://www.googleapis.com/youtube/v3/videos'
-
-    search_params = {
-        'part': 'snippet',
-        'q': 'learn django',
-        'key': settings.YOUTUBE_DATA_API_KEY,
-        'maxResults': 9,
-        'type': 'video',
-    }
-
-    video_ids = []
-    r = requests.get(search_url, params=search_params)
-
-    results = r.json()['items']
-
-    for result in results:
-        video_ids.append(result['id']['videoId'])
-
-    video_params = {
-        'key': settings.YOUTUBE_DATA_API_KEY,
-        'part': 'snippet, contentDetails',
-        'id': ','.join(video_ids),
-        'maxResults': 9,
-    }
-
-    r = requests.get(video_url, params=video_params)
-
-    results = r.json()['items']
-
     videos = []
-    for result in results:
-        video_data = {
-            'title': result['snippet']['title'],
-            'id': result['id'],
-            'duration': parse_duration(result['contentDetails']['duration']).total_seconds() // 60,
-            'thumbnail': result['snippet']['thumbnails']['high']['url']
+    if request.method == 'POST':
+        search_url = 'https://www.googleapis.com/youtube/v3/search'
+        video_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+        search_params = {
+            'part': 'snippet',
+            'q': request.POST['search'],
+            'key': settings.YOUTUBE_DATA_API_KEY,
+            'maxResults': 9,
+            'type': 'video',
         }
-        videos.append(video_data)
+
+        r = requests.get(search_url, params=search_params)
+
+        results = r.json()['items']
+
+        video_ids = []
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+
+        if request.POST['submit'] == 'lucky':
+            return redirect(f'https://www.youtube.com/watch?v={video_ids[0]}')
+
+        video_params = {
+            'key': settings.YOUTUBE_DATA_API_KEY,
+            'part': 'snippet, contentDetails',
+            'id': ','.join(video_ids),
+            'maxResults': 9,
+        }
+
+        r = requests.get(video_url, params=video_params)
+
+        results = r.json()['items']
+
+        for result in results:
+            video_data = {
+                'title': result['snippet']['title'],
+                'id': result['id'],
+                'url': f'https://www.youtube.com/watch?v={result["id"]}',
+                'duration': int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
+                'thumbnail': result['snippet']['thumbnails']['high']['url']
+            }
+            videos.append(video_data)
     return render(request, 'youtube/home.html', {'videos': videos})
